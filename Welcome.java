06a32851724d194f932232cca26391aadf516e9d
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-09-23 08:53:27 +0530

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.Hotel;
+
+/**
+ * @author RUSHIKESHBHOSLE
+ *
+ */
+public class HotelReservationSyatem {
+	
+public static void main(String[] args) {
+	System.out.println("Welcome to Hotel Reservation System");
+}
+
+}
